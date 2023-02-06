aaa41e0b034fe838350be75bb5d97bc585475677
jenmwa <jenmwa@gmail.com>
2023-02-06 23:54:32 +0100

    :lipstick: socials added

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -5,12 +5,18 @@
     <span class="material-symbols-outlined">waving_hand</span>
     <p class="about-me-text">I'm Jenny, Nice to meet you! </p>
     <p class="about-me-text">I’m a Front-End Developer Student at Medieinstitutet in Stockholm, Sweden and I really enjoy playing around with colors, fonts, images, taking photos, being creative, and make websites come to life with both design and functionality.</p>
-    <p class="about-me-text">Right now I’m looking for LIA for the fall 2023!
+    <p class="about-me-text">Right now I’m looking for LIA for the fall 2023!<br>
       I’m looking for a company where I will be a part of a team and where I could continue grow in my role as a Front-End developer.</p>
     <p>Should we talk?<br> Let’s connect!</p>
-    <img class="icons" src="/linkedin-fill-svgrepo-com.svg">  
-    <p class="meaning-of-life">Try and be nice to people, avoid eating fat, read a good book every now and then, get some walking in, and try and live together in peace and harmony with people of all creeds and nations</p>
-    <p>and for everybody's sake, KEEP IT SIMPLE!</p>
+
+    <img class="icons" src="/linkedin-fill-svgrepo-com.svg">
+    <img class="icons" src="/email-svgrepo-com.svg">
+    <img class="icons" src="/github-svgrepo-com.svg">
+
+    <div class="words">
+      <p class="meaning-of-life">Try and be nice to people, avoid eating fat, <br>read a good book every now and then,<br>get some walking in, <br>and try and live together in peace and harmony with people of all creeds and nations</p>
+      <p>and for everybody's sake, KEEP IT SIMPLE!</p>
+    </div>
     <span class="material-symbols-outlined rocket">
 rocket_launch
 </span>
@@ -47,8 +53,9 @@ h2 {
   text-transform: none;
   }
 
-  .icons {
+  .icons, .material-symbols-outlined {
     margin-top: 1rem;
+    margin-right: 1rem;
     width: 35px;
     filter: invert(72%) sepia(82%) saturate(366%) hue-rotate(330deg) brightness(98%) contrast(92%);
     transition: transform 500ms;
@@ -62,12 +69,19 @@ h2 {
     }
     
   }
-  .meaning-of-life {
-    text-transform: none;
-    font-family: $secondaryFont;
-    width: 60%;
-    font-style: italic;
-
+  .words {
+    text-align: right;
+    background: none;
+    height: 0; 
+  
+    .meaning-of-life {
+      margin-left: auto;
+      padding-left: 1rem;
+      text-transform: none;
+      font-family: $secondaryFont;
+      width: 70%;
+      font-style: italic;
+    }
   }
   .rocket {
     font-size: 4rem;
