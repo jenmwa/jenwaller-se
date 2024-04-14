e2d8d78e3c149c0c9cc9226bec2b22a2d194c49d
jenmwa <jenmwa@gmail.com>
2024-04-14 13:41:13 +0200

    comment added for future deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/jenwaller-se/',
+  base: '/jenwaller-se/', //github
+  // base: '/', //jenwaller.se
   define: {
     'process.env': {},
   },
